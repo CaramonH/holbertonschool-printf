d9a3c849bf3709abbd7d80bd4314de441cd8aa6f
CaramonH <caramonh@yahoo.com>
2023-03-08 11:13:41 -0800

    main

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - prints output according to format
+ * @format: format string to print
+ * Return: int number of character printed
+ */
+int _printf(const char *format, ...)
+{
+	int i, j, count = 0;
+}
--- main.h
+++ main.h
@@ -9,14 +9,22 @@
 #include <stdarg.h>
 
 /* STRUCTS */
-
 typedef struct print
 {
 	char *print;
 	void (*f)(char*, ...)
-} print_t;
+} print_f;
 
 /* FUNCTION PROTOTYPES */
+/* str.c */
+char *spr(va_list);
+
+/* int.c */
+char *dipr(va_list);
+
+/* char.c */
+char *cpr(va_list);
+
 /* _printf.c */
 int _printf(const char *format, ...);
 
