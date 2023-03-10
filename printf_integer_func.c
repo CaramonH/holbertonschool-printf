73be9e87181d665472b4de4ee2df652411cfbf48
Jolsen6 <olsenjeremiah06@gmail.com>
2023-03-10 01:35:27 -0800

    hello friend

---
+++ printf_integer_func.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_i - function
+* @i: integer
+* Description: prints an integer
+* Return: number chars and digits
+*/
+
+int print_i(va_list i)
+
+{
+int t[10];
+int var1, op1, f, sum, count;
+
+f = va_arg(i, int);
+
+I wish caramon was here to tell me I did this wrong XD
